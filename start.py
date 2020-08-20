ce4bf5c5e4f426ea41d98dd4e68ed53bcd3dcf26
Elfi Yang <elfiyang16@gmail.com>
2020-08-20 15:38:19 +0100

    feat: sessions

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "elfi"
 
 
 @app.route("/")
@@ -8,15 +9,10 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-
-
-@app.route("/admin/")
-def admin():
-    # redirect to user and pass in name arg as Admin!
-    return redirect(url_for("user", name="Admin!"))
+# @app.route("/admin/")
+# def admin():
+#     # redirect to user and pass in name arg as Admin!
+#     return redirect(url_for("user", name="Admin!"))
 
 
 @app.route("/login", methods=["POST", "GET"])
@@ -24,14 +20,27 @@ def login():
     if request.method == "POST":
       # comes in as a dictionary
         user = request.form["name"]
-        return redirect(url_for("user", usr=user))
+        session["user"] = user
+        return redirect(url_for("user"))
     else:
-        return render_template("/login.html")
+        if "user" in session:
+            return redirect(url_for("user"))
+        return render_template("login.html")
 
 
-@app.route("/<usr>")
-def iser(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
