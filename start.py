3401b7e98cc1c9ed7a141fc2a213409b69eb9e50
Elfi Yang <elfiyang16@gmail.com>
2020-08-20 14:54:31 +0100

    feat: post and get in python

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -17,6 +17,21 @@ def user(name):
 def admin():
     # redirect to user and pass in name arg as Admin!
     return redirect(url_for("user", name="Admin!"))
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+      # comes in as a dictionary
+        user = request.form["name"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("/login.html")
+
+
+@app.route("/<usr>")
+def iser(usr):
+    return f"<h1>{usr}</h1>"
 
 
 if __name__ == "__main__":
