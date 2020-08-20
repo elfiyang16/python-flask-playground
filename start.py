bc2b3312aef6ec45551cd65f80f8927ec5f0a36b
Elfi Yang <elfiyang16@gmail.com>
2020-08-20 14:34:03 +0100

    feat: bootstrap and template inheritance

--- start.py
+++ start.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", content=["elfi", "julia"])
+    return render_template("index.html")
 
 
 @app.route("/<name>")
@@ -20,4 +20,4 @@ def admin():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
