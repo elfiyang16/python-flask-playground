defa64248cae385ca39df81e5e02d16d8272169e
Elfi Yang <elfiyang16@gmail.com>
2020-08-21 14:41:14 +0100

    feat: add sql database

--- start.py
+++ start.py
@@ -1,10 +1,25 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "elfi"
 # permanent session
 app.permanent_session_lifetime = timedelta(minutes=15)
+app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///users.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.String(100))
+    email = db.Column("email", db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 
 @app.route("/")
@@ -25,6 +40,14 @@ def login():
       # comes in as a dictionary
         user = request.form["name"]
         session["user"] = user
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.session.commit()
+
         flash("Login Successfully")
         return redirect(url_for("user"))
     else:
@@ -33,11 +56,22 @@ def login():
         return render_template("login.html")
 
 
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        return render_template("login.html", user=user)
+        if request.method == "POST":
+            email = request.form["email"]
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+            # session["email"] = email
+            flash("Email was saved")
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html", email=email)
     else:
         return redirect(url_for("login"))
 
@@ -45,9 +79,17 @@ def user():
 @app.route("/logout")
 def logout():
     session.pop("user", None)
+    session.pop("email", None)
+
     flash("you have been logged out!", "info")
     return redirect(url_for("login"))
 
 
+@app.route("/view")
+def view():
+    return render_template("view.html", values=users.query.all())
+
+
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
